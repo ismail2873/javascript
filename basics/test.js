7de9844c37db5331b74557ab613ec9f403e69f32
ismail niazi <138701546+ismail2873@users.noreply.github.com>
2025-04-29 14:09:50 +0000

    pre

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,38 @@
-console.log("tested sucessfully");
+// primative data type
+
+
+
+
+// 7 types [string,null,number,boolean,undefined,symbol,bigint]
+
+
+const id = Symbol('1234')
+const anotherId =Symbol('1234')
+console.log (id==anotherId)
+
+//Reference [non permitive data type]
+
+// Array,function,Object
+
+let myObj={
+    name :'xyz',
+    age :22
+}
+console.log(myObj)
+
+
+
+//function
+
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(myFunction)
+
+//how to check type
+
+
+console.log(typeof myFunction);
+
