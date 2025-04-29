2b999c1665df7305eeb0df6572fa40db968a3a06
ismail niazi <138701546+ismail2873@users.noreply.github.com>
2025-04-29 18:20:08 +0000

    string chages

--- basics/String.js
+++ basics/String.js
@@ -49,3 +49,19 @@ const url ="http://ismailniazi.com/gudyrs%20gu)"
 console.log(url.replace("%20" , "_"))
 console.log(url.includes("niazi"));
 console.log(gameName.split("-"));
+
+
+//number & Maths
+
+
+const score =400
+console.log(score);
+
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+
+
