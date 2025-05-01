b31dbf94e32f91db5738710c7a99f367b267f79f
ismail niazi <138701546+ismail2873@users.noreply.github.com>
2025-05-01 18:33:38 +0000

    array changes

---
+++ basics/array.js
@@ -0,0 +1,20 @@
+const myArray = [1,2,3,4,5]
+const myArray2 = new Array(1,2,3,4,56,67,)
+console.log(myArray[1])
+console.log(myArray2)
+
+
+//array methods
+
+
+myArray.push(8);
+console.log(myArray)
+
+myArray.pop();
+console.log(myArray)
+myArray.unshift(0);
+console.log(myArray)
+myArray.shift();
+console.log(myArray)
+myArray.splice(2, 1);
+console.log(myArray)
