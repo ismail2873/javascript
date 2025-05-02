6f231fe2b9c1d4e752f0fa33a5d5d538411cbca1
ismail niazi <138701546+ismail2873@users.noreply.github.com>
2025-05-02 18:14:55 +0000

    object keys values and entries

--- basics/2-objects.js
+++ basics/2-objects.js
@@ -4,7 +4,7 @@ const newUser ={}
 newUser.id = "123444"
 newUser.name = "xyz"
 newUser.isLoggedIn = false
-console.log(newUser);
+// console.log(newUser);
 
 
 
@@ -22,4 +22,46 @@ const regularUser = {
     }
 
 }
-console.log(regularUser.fullName.userFullName.firstName);
+// console.log(regularUser.fullName.userFullName.firstName);
+
+
+const obj1 = {
+    1:"a",2:"b",3:"c"
+}
+const obj2 = {
+    4:"d",5:"e",6:"f"
+}
+// const obj3 = {obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+
+
+const UserData = [
+    {
+        id:1,
+        name:"9hiuig",
+        email:"eonowni@gmail.com"
+    },
+    {
+        id:2,
+        name:"9h",
+        email:"ei@gmail.com"
+    },
+    {
+        id:3,
+        name:"iig",
+        email:"owni@gmail.com"
+    },
+    {
+        id:4,
+        name:"9hig",
+        email:"eono@gmail.com"
+    },
+]
+
+UserData[1].email
+console.log(UserData);
+console.log(Object.keys(UserData));
+console.log(Object.values(UserData));
+console.log(Object.entries(UserData));
